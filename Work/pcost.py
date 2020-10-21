c178fd2ba76d9a16c6ebcace9e3c17b54408862a
jianye <18817571704@163.com>
2020-10-21 16:08:20 +0200

    "List and String"

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,22 @@
 # pcost.py
 #
 # Exercise 1.27
+
+import os
+os.getcwd()
+# with open('/home/jianye/practical-python/Work/Data/portfolio.csv','rt') as f:
+#     data = f.read()
+# print(data)
+
+lists=[]
+with open('/home/jianye/practical-python/Work/Data/portfolio.csv','rt') as f:
+    headers = next(f)
+    for line in f:   
+        lists.append(line.split(',')) 
+        print(line.split(','))
+
+print(lists)
+sum = 0
+for list in lists:
+    sum= sum+ int(list[1])*float(list[2])
+print(sum)
