f74caee5c40a2e25832aa469ee79c1bf4ce32891
jianye <18817571704@163.com>
2020-10-25 20:30:31 +0100

    '2.6 collections'

--- Work/exercise.py
+++ Work/exercise.py
@@ -33,11 +33,62 @@ import sys
 #     except ValueError:
 #         print(f'Row {rowno}: Bad Row: {row}')
 
-f=open('/home/jianye/practical-python/Work/Data/portfolio.csv')
-rows = csv.reader(f)
-headers = next(rows)
-print(headers)
-row = next(rows)
-print(row)
-print(list(zip(headers,row)))#list
-print(dict(zip(headers,row)))#dict
+# f=open('/home/jianye/practical-python/Work/Data/portfolio.csv')
+# rows = csv.reader(f)
+# headers = next(rows)
+# print(headers)
+# row = next(rows)
+# print(row)
+# print(list(zip(headers,row)))#list
+# print(dict(zip(headers,row)))#dict
+
+portfolio = [
+    ('GOOG', 100, 490.1),
+    ('IBM', 50, 91.1),
+    ('CAT', 150, 83.44),
+    ('IBM', 100, 45.23),
+    ('GOOG', 75, 572.45),
+    ('AA', 50, 23.15)
+]
+
+from collections import Counter
+
+total_shares = Counter()
+
+for name, shares, price in portfolio:
+    total_shares[name] += shares
+# print(total_shares['IBM'])#150
+
+
+# one-many mapping
+from collections import defaultdict
+
+holdings = defaultdict(list)
+
+for name,shares,price in portfolio:
+    holdings[name].append((shares,price))
+# print(holdings['IBM'])
+
+from collections import deque
+# history = deque(maxlen=N)
+# with open(filename) as f:
+#     for line in f:
+#         history.append(line)
+#         ...
+
+
+#List Comprehension
+#1
+# a = [1,2,3,4,5]
+# b = [2*x for x in a]
+# print(b)
+
+#2
+# names = ['Elwood','Jake']
+# a = [name.lower() for name in names]
+# print(a)
+
+#3 Filtering
+# a = [1,-5,4,2,-2,10]
+# b = [2*x for x in a if x > 0]
+# print(b) 
--- Work/report.py
+++ Work/report.py
@@ -116,3 +116,82 @@ def make_a_new_report(filename):
 
 filename = '/home/jianye/practical-python/Work/Data/portfoliodate.csv'
 print(make_a_new_report(filename))
+
+
+portfolio = read_portfolio_to_dict('/home/jianye/practical-python/Work/Data/portfolio.csv')
+
+from collections import Counter
+holdings = Counter()
+
+for s in portfolio:
+    holdings[s['name']] += s['shares']
+# print('Holdings-shares',holdings)
+# print('IBM',holdings['IBM'])
+# print('Most common',holdings.most_common(3))
+
+portfolio2 = read_portfolio_to_dict('/home/jianye/practical-python/Work/Data/portfolio2.csv')
+
+holding2 = Counter()
+
+for s in portfolio2:
+    holding2[s['name']]+=s['shares']
+# print(holding2)
+
+combind = holdings + holding2
+# print('Combined',combind)
+
+portfolio = read_portfolio_to_dict('/home/jianye/practical-python/Work/Data/portfolio.csv')
+cost = sum([s['shares']*s['price'] for s in portfolio])
+# print(cost)
+
+
+
+list = [s['shares']*s['price'] for s in portfolio]
+# print(list)
+
+more100 = [s for s in portfolio if s['shares']>100]
+# print(more100)
+
+msftibm = [s for s in portfolio if s['name'] in {'IBM','MSFT'}]
+# print(msftibm)
+
+
+cost10k = [s for s in portfolio if s['shares']*s['price']>10000]
+# print(cost10k)
+
+
+name_shares = [(s['name'],s['shares']) for s in portfolio]
+# print(name_shares)
+
+names = {s['name'] for s in portfolio}
+# print(names)
+
+holdings ={name:0 for name in names}
+# print(holdings)
+
+for s in portfolio:
+    holdings[s['name']] += s['shares']
+# print(holdings)
+
+import csv
+f = open('/home/jianye/practical-python/Work/Data/portfoliodate.csv')
+rows = csv.reader(f)
+# print(rows)
+headers = next(rows)
+# print(headers)
+
+
+select = ['name','shares','price']
+indices = [headers.index(colname) for colname in select]
+
+# print(indices)
+
+row = next(rows)
+record = {colname:row[index] for colname, index in zip(select, indices)}
+# print(record)
+
+
+portfolio = [{colname:row[index] for colname, index in zip(select,indices)}for row in rows]
+print(portfolio)
+
+
