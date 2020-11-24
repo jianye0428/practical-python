eb45f8741ce4a3b4df7063aae4eed165636aae93
jianye <18817571704@163.com>
2020-11-24 08:25:58 +0100

    ‘9.2-9.3’

---
+++ Work/porty-app/setup.py
@@ -0,0 +1,13 @@
+# setup.py
+
+import setuptools
+
+setuptools.setup(
+    name="porty", 
+    version="0.0.1",
+    author="Jian Ye",
+    author_email="jianye0428@gmail.com",
+    description="Practical Python Code",
+    packages=setuptools.find_packages(),
+    scripts=['print-report.py'],
+)
